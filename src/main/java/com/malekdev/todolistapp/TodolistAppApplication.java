a0ce1ca8d10f610646de74262100ec8d1ab6711f
abdulmalikalomayri <aomayri@cerebra.sa>
2022-02-26 19:43:26 +0300

    init

---
+++ src/main/java/com/malekdev/todolistapp/TodolistAppApplication.java
@@ -0,0 +1,13 @@
+package com.malekdev.todolistapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malekdev/todolistapp/TodolistAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malekdev.todolistapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
