c475383e2061094be68e1c880ab7d14964031e86
Abdulmalik <omayri970@gmail.com>
2023-12-01 15:53:55 +0300

    add join table

--- src/main/java/com/malek/app/rest/Model/UserEntity.java
+++ src/main/java/com/malek/app/rest/Model/UserEntity.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "users")
 @Data
@@ -17,4 +20,10 @@ public class UserEntity {
     private String username;
 
     private String password;
+
+    // create join table user_role
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<Role> role = new ArrayList<>();
 }
