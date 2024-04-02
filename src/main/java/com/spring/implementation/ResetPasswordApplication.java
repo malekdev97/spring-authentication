f849fadf40f494759aca3237f35a041728b6c9d2
Abdulmalik <abdulmalik@Abdulmaliks-MacBook-Pro.local>
2024-04-02 13:50:26 +0300

    add home page

--- src/main/java/com/spring/implementation/ResetPasswordApplication.java
+++ src/main/java/com/spring/implementation/ResetPasswordApplication.java
@@ -2,16 +2,24 @@ package com.spring.implementation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @SpringBootApplication
+@Controller
 public class ResetPasswordApplication {
 
 	public static void main(String[] args) {
 		 SpringApplication.run(ResetPasswordApplication.class, args);
+		
+	}
 
+	@GetMapping("/")
+	public String home() {
+		return "home";
 	}
 
 }
--- src/main/java/com/spring/implementation/model/Job.java
+++ src/main/java/com/spring/implementation/model/Job.java
@@ -1,10 +1,12 @@
 package com.spring.implementation.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Job {
     
     @Id
